0c446f46b6f510d5509982aa75da0370dc9cd9d7
Alex Hotsaliyk <artoasmith@gmail.com>
2016-08-10 17:35:03 +0300

    sectionMenu

--- admin_rgk/src/RgkBundle/Controller/PriceController.php
+++ admin_rgk/src/RgkBundle/Controller/PriceController.php
@@ -11,6 +11,7 @@ namespace RgkBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use RgkBundle\Entity\Section;
 
 class PriceController extends BaseController
 {
@@ -19,10 +20,37 @@ class PriceController extends BaseController
      */
     public function indexAction(Request $request)
     {
+        /**
+         * @var Section $a
+         */
         $params = $this->getTemplatteParams();
-        //var_dump($request->get('_route'));
-        //exit();
-        return $this->render('RgkBundle:Admin:base.html.twig',$params);
+        $params['sections'] = $this->getDoctrine()
+                                     ->getRepository('RgkBundle:Section')
+                                     ->findBy(array(), array('title' => 'ASC'));
+        $params['sections'] = $this->menuStrict($params['sections']);
+        return $this->render('RgkBundle:Admin:price.html.twig',$params);
     }
 
+    private function menuStrict(&$objects,$parent=0)
+    {
+
+        $resp = [];
+        /**
+         * @var Section $object
+         */
+        if(!empty($objects)){
+            foreach ($objects as &$object){
+                $cPar = ($object->getParentSection()?$object->getParentSection()->getId():0);
+                if($cPar != $parent)
+                    continue;
+
+                $resp[] = [
+                    'id'=>$object->getId(),
+                    'title'=>$object->getTitle(),
+                    'children'=>$this->menuStrict($objects,$object->getId())
+                ];
+            }
+        }
+        return $resp;
+    }
 }
